58abb9569b7e08934b59787490c63686277202ad
huntoor <omarahmed99929@gmail.com>
2024-02-26 18:23:40 +0200

    removeing unnecessary code

--- main/main.gd
+++ main/main.gd
@@ -3,16 +3,6 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	# new_game()
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func game_over():
 	$DeathSound.play()
 	$ScoreTimer.stop()
